36aedf920d9e77d2e9556cdb5f2a92293db7b7ab
Javvadhi Dhanush <dhanushj217@gmail.com>
2025-03-14 15:47:24 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+export const API_URL = "https://backend-69el.onrender.com/";
 
 // https://vite.dev/config/
 export default defineConfig({
