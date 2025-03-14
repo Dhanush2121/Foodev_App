6a7e829096616d7e50e505ac28e3997594a0d418
Javvadhi Dhanush <dhanushj217@gmail.com>
2025-03-14 19:20:18 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://backend-69el.onrender.com/"
 
   return (
     <div>
